b61bc57c22cd1b1b7d44e5ff171bfe54d2a1749a
TekeJane <teke.faithjane@ictuniversity.edu.cm>
2026-03-26 12:15:56 +0100

    fix: correct products table name in preorder recovery migration

--- migrations/20260316094500-recover-preorder-columns.js
+++ migrations/20260316094500-recover-preorder-columns.js
@@ -1,9 +1,9 @@
 module.exports = {
   async up(queryInterface, Sequelize) {
-    const table = await queryInterface.describeTable('products');
+    const table = await queryInterface.describeTable('Products');
 
     if (!table.is_preorder) {
-      await queryInterface.addColumn('products', 'is_preorder', {
+      await queryInterface.addColumn('Products', 'is_preorder', {
         type: Sequelize.BOOLEAN,
         allowNull: false,
         defaultValue: false,
@@ -11,14 +11,14 @@ module.exports = {
     }
 
     if (!table.preorder_days) {
-      await queryInterface.addColumn('products', 'preorder_days', {
+      await queryInterface.addColumn('Products', 'preorder_days', {
         type: Sequelize.INTEGER,
         allowNull: true,
       });
     }
 
     if (!table.preorder_available_date) {
-      await queryInterface.addColumn('products', 'preorder_available_date', {
+      await queryInterface.addColumn('Products', 'preorder_available_date', {
         type: Sequelize.DATE,
         allowNull: true,
       });
@@ -26,8 +26,8 @@ module.exports = {
   },
 
   async down(queryInterface) {
-    await queryInterface.removeColumn('products', 'preorder_available_date');
-    await queryInterface.removeColumn('products', 'preorder_days');
-    await queryInterface.removeColumn('products', 'is_preorder');
+    await queryInterface.removeColumn('Products', 'preorder_available_date');
+    await queryInterface.removeColumn('Products', 'preorder_days');
+    await queryInterface.removeColumn('Products', 'is_preorder');
   },
 };
